183fa913b34dccd0d056cee57d615a49de9f8bc2
Oskar Lind <oskar.lind@gmail.com>
2020-08-19 13:51:31 +0200

    Uppdaterad inför övning 16A

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 //console.log("Hello from scripts.js")
 
+let recipes = [];
+
+
+
 /**
  * Slider for recipes
  */
@@ -31,21 +35,35 @@ let calculateQuantity = function(originalAmount, multiplier) {
 /**
  * Constructor for Car model
  */
-function Car(year, make, color) {
-    this.year = year;
-    this.make = make;
-    this.color = color;
+function Car(...properties) {
+    this.year = properties[1];
+    this.make = properties[2];
+    this.color = properties[3];
 }
 let car1 = new Car(1991, "VW", "red");
+car1.isInsured = false;
 let car2 = new Car(1997, "Fiat", "yellow");
 
 /**
  * Recipe constructor
  */
-function Recipe(title, ingredients) {
+function Recipe(title, ingredients, portions, isVegan, difficulty) {
     this.title = title;
     this.ingredients = ingredients;
+    this.portions = portions;
+    this.isVegan = isVegan;
+    this.difficulty = difficulty;
 }
-let applePie = new Recipe("Apple pie", ["Apples", "Flour", "Butter"]);
-let blueberrySmoothie = new Recipe("Blueberry smoothie", ["Bluberries", "Sugar"]);
-const recipes = [applePie, blueberrySmoothie];
+let applePie = new Recipe("Apple pie", ["Apples", "Flour", "Butter"], 4, false, 3);
+let blueberrySmoothie = new Recipe("Blueberry smoothie", ["Bluberries", "Sugar"], 2, true, 4);
+let mashedPotatoes = new Recipe("Mashed potatoes", ["Potatoes", "Butter"], 8, false, 1);
+
+recipes.push(applePie);
+recipes.push(blueberrySmoothie);
+recipes.push(mashedPotatoes);
+
+let removeLastRecipe = () => {
+    recipes.pop();
+    document.getElementById("availableRecipes").innerHTML = "";
+    recipes.forEach(renderRecipes);
+}
